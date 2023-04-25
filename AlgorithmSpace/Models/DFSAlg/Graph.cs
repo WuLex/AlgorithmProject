9b92fb963f7748a4187a93c415929d20d3f47aa9
wulex <827937686@qq.com>
2023-04-26 01:26:05 +0800

    add bfs

---
+++ AlgorithmSpace/Models/DFSAlg/Graph.cs
@@ -0,0 +1,106 @@
+﻿namespace AlgorithmSpace.Models.DFSAlg
+{
+    /// <summary>
+    /// 广度优先遍历
+    ///
+    /// 在遍历过程中，先将起始节点加入到 visited 和 queue 中，
+    /// 然后通过不断出队列的方式遍历队列中的节点，并将其邻居节点加入到队列中
+    /// </summary>
+    internal class Graph
+    {
+        private Dictionary<int, List<int>> adjacencyList;
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<int, List<int>>();
+        }
+
+        // 添加节点和其邻居
+        public void AddNode(int node, List<int> neighbours)
+        {
+            adjacencyList[node] = neighbours;
+        }
+
+        // 广度优先搜索遍历
+        public void BFS(int start)
+        {
+            //用于存储已访问过的节点的哈希集合
+            HashSet<int> visited = new HashSet<int>();
+            //用于存储待访问节点的队列
+            Queue<int> queue = new Queue<int>();
+
+            // 标记当前节点为已访问
+            visited.Add(start);
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                // 从队列中取出下一个待访问节点
+                int current = queue.Dequeue();
+                Console.Write(current + " "); // 输出当前节点
+                if (adjacencyList.ContainsKey(current)) // 检查当前节点是否有邻居节点
+                {
+                    // 遍历当前节点的邻居节点列表
+                    foreach (var neighbour in adjacencyList[current])
+                    {
+                        // 检查邻居节点是否已被访问过
+                        if (!visited.Contains(neighbour))
+                        {
+                            visited.Add(neighbour);// 将未访问过的邻居节点添加到已访问节点集合中
+                            queue.Enqueue(neighbour);// 将未访问过的邻居节点加入到待访问节点队列中，以便后续访问其邻居节点
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    /// <summary>
+    /// 深度优先遍历
+    /// </summary>
+    //class Graph
+    //{
+    //    private Dictionary<int, List<int>> adjacencyList;
+
+    //    public Graph()
+    //    {
+    //        adjacencyList = new Dictionary<int, List<int>>();
+    //    }
+
+    //    // 添加节点和其邻居
+    //    public void AddNode(int node, List<int> neighbours)
+    //    {
+    //        adjacencyList[node] = neighbours;
+    //    }
+
+    //    // 深度优先搜索遍历
+    //    public void DFS(int start, HashSet<int> visited)
+    //    {
+    //        // 如果节点已被访问过，则直接返回
+    //        if (visited.Contains(start))
+    //        {
+    //            return;
+    //        }
+
+    //        // 标记当前节点为已访问
+    //        visited.Add(start);
+    //        Console.Write(start + " "); // 输出当前节点
+
+    //        // 递归访问邻居节点
+    //        foreach (var neighbour in adjacencyList[start])
+    //        {
+    //            DFS(neighbour, visited);
+    //        }
+    //    }
+
+    //    // 搜索图并输出
+    //    public void SearchGraph(int start)
+    //    {
+    //        HashSet<int> visited = new HashSet<int>();
+    //        DFS(start, visited);
+    //        Console.WriteLine();
+    //    }
+    //}
+}
--- AlgorithmSpace/Program.cs
+++ AlgorithmSpace/Program.cs
@@ -1,28 +1,49 @@
 ﻿using AlgorithmSpace;
+using AlgorithmSpace.Models.DFSAlg;
 using AlgorithmSpace.Models.HuffmanAlg;
 
 #region 二分法查找
-int[] arr = { 1, 3, 5, 7, 9 };
-int target = 5;
-int result = BinarySearch.Search(arr, target);
+//int[] arr = { 1, 3, 5, 7, 9 };
+//int target = 5;
+//int result = BinarySearch.Search(arr, target);
 
-if (result == -1)
-{
-    Console.WriteLine("Element not found in array");
-}
-else
-{
-    Console.WriteLine("Element found at index " + result);
-}
+//if (result == -1)
+//{
+//    Console.WriteLine("Element not found in array");
+//}
+//else
+//{
+//    Console.WriteLine("Element found at index " + result);
+//}
 #endregion
 
 #region 哈夫曼树使用示例
 // 使用示例
-var originalText = "This is a test string to be compressed using Huffman encoding";
-var compressedBytes = HuffmanCompressor.Compress(originalText);
-var compressedText = Convert.ToBase64String(compressedBytes);
-var decompressedText = HuffmanCompressor.Decompress(Convert.FromBase64String(compressedText));
-Console.WriteLine(originalText);
-Console.WriteLine(compressedText);
-Console.WriteLine(decompressedText);
+//var originalText = "This is a test string to be compressed using Huffman encoding";
+//var compressedBytes = HuffmanCompressor.Compress(originalText);
+//var compressedText = Convert.ToBase64String(compressedBytes);
+//var decompressedText = HuffmanCompressor.Decompress(Convert.FromBase64String(compressedText));
+//Console.WriteLine(originalText);
+//Console.WriteLine(compressedText);
+//Console.WriteLine(decompressedText);
 #endregion
+
+
+#region 搜索图
+// 构建网状图
+Graph graph = new Graph();
+graph.AddNode(1, new List<int> { 2, 3, 4 });
+graph.AddNode(2, new List<int> { 1, 5 });
+graph.AddNode(3, new List<int> { 1, 5 });
+graph.AddNode(4, new List<int> { 1, 6 });
+graph.AddNode(5, new List<int> { 2, 3, 7 });
+graph.AddNode(6, new List<int> { 4, 7 });
+graph.AddNode(7, new List<int> { 5, 6 });
+
+// 输出网状图
+// 输出网状图
+Console.WriteLine("BFS traversal:");
+graph.BFS(1); // 从节点1开始广度优先搜索并输出
+#endregion
+
+
